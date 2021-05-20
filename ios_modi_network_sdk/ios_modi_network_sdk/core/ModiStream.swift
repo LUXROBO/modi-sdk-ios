24b5c457b89f16e6ad24841b82ed7ddc3ce026e1
steave <kstlove123@gmail.com>
2021-05-20 11:22:43 +0900

    ModiStream 멤버변수 접근제한 수정

--- ios_modi_network_sdk/ios_modi_network_sdk/core/ModiStream.swift
+++ ios_modi_network_sdk/ios_modi_network_sdk/core/ModiStream.swift
@@ -7,24 +7,26 @@
 
 import Foundation
 
-class ModiStream : NSObject {
+
+class ModiStream {
     
-    enum STREAM_TYPE {
-        case INTERPRETER
+    enum STREAM_TYPE : Int {
+        case INTERPRETER = 3
     }
     
-    enum STREAM_RESPONSE {
-        case SUCCESS
-        case DUPLICATE
-        case UNDEFINED
-        case TIMEOUT
+    enum STREAM_RESPONSE : Int {
+        case SUCCESS = 0
+        case DUPLICATE = 3
+        case UNDEFINED = 4
+        case TIMEOUT = 5
     }
     
-    private var moduleId : Int = 0
-    private var streamId : UInt8 = 0
-    private var streamType : STREAM_TYPE = STREAM_TYPE.INTERPRETER
-    private var streamBody : Array<UInt8> = []
+    var moduleId : Int = 0
+    var streamId : UInt8 = 0
+    var streamType : STREAM_TYPE = STREAM_TYPE.INTERPRETER
+    var streamBody : Array<UInt8> = []
         
+    
     func makeStream(moduleId : Int, streamId : Int, streamType : STREAM_TYPE, streamBody : Array<UInt8>) -> Self {
         
         self.moduleId = moduleId
