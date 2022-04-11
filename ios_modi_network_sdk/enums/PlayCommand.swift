4d8d87e1563fab78a7ca4127dbcdff4abcaecb9f
kstlove <kstlove123@gmail.com>
2022-04-11 11:50:55 +0900

    PlayCommand 수정
    - IMU 프로퍼티 추가
    - 모디 플러스에 맞게 command 수정

--- ios_modi_network_sdk/enums/PlayCommand.swift
+++ ios_modi_network_sdk/enums/PlayCommand.swift
@@ -16,25 +16,32 @@ public enum PlayCommand  {
     case BUTTON_CLICK
     case BUTTON_DOUBLE_CLICK
     case TOGGLE
-    case JOYSTICK
-    case DIAL
-    case LEFT_SLIDER
-    case RIGHT_SLIDER
+    case JOYSTICK_DIRECTION
+    case DIAL_POSITION
+    case SLIDER_POSITION
     case TIMER
-    
+    case IMU_ANGLE_ROLL
+    case IMU_ANGLE_PITCH
+    case IMU_ANGLE_YAW
+    case IMU_DIRECTION
+    case IMU_ROTATION
     
     public var rawValue: RawValue {
       switch self {
-        case .RECEIVE_DATA: return 0x0002
-        case .BUTTON_PRESS_STATUS: return 0x0003
-        case .BUTTON_CLICK: return 0x0003
-        case .BUTTON_DOUBLE_CLICK: return 0x0003
-        case .TOGGLE: return 0x0004
-        case .JOYSTICK: return 0x0006
-        case .DIAL: return 0x0005
-        case .LEFT_SLIDER: return 0x0007
-        case .RIGHT_SLIDER: return 0x0006
-        case .TIMER: return 0x0008
+        case .RECEIVE_DATA: return 0x02
+        case .BUTTON_PRESS_STATUS: return 0x00
+        case .BUTTON_CLICK: return 0x02
+        case .BUTTON_DOUBLE_CLICK: return 0x04
+        case .TOGGLE: return 0x00
+        case .JOYSTICK_DIRECTION: return 0x00
+        case .DIAL_POSITION: return 0x00
+        case .SLIDER_POSITION: return 0x00
+        case .TIMER: return 0x00
+        case .IMU_ANGLE_ROLL: return 0x00
+        case .IMU_ANGLE_PITCH : return 0x02
+        case .IMU_ANGLE_YAW : return 0x04
+        case .IMU_DIRECTION : return 0x00
+        case .IMU_ROTATION : return 0x00
        
       }
     }
